4adf39f8132e1a369f13d4b8ccff8e6037f69e28
Vince Jerald Villamora <vincevillamora@gmail.com>
2023-08-30 22:10:33 +0800

    fix(api): remove `timestamp` from `ApiRequest` 2

--- src/main/java/com/vincejv/m360/dto/ApiRequest.java
+++ src/main/java/com/vincejv/m360/dto/ApiRequest.java
@@ -1,19 +1,13 @@
 package com.vincejv.m360.dto;
 
-import java.time.LocalDateTime;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 @JsonInclude(Include.NON_NULL)
 public class ApiRequest {
-
-  private LocalDateTime timestamp;
 
 }
